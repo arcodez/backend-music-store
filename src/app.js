861121a16faf479c28cf46656249f2db319649ab
arcodez <abelileitor123@gmail.com>
2021-08-13 13:09:43 -0400

    update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
-const { urlencoded } = require("express");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(urlencoded);
 
 app.get("/", (req, res) => {
   res.send("Porti Backend Music Store");
