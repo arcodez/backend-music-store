8bdb4f1d2c784836f737778fc0c7ff5e9dea4a98
arcodez <abelileitor123@gmail.com>
2021-08-13 13:00:08 -0400

    update

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
+const { urlencoded } = require("express");
 const express = require("express");
 const app = express();
+
+app.use(express.json());
+app.use(urlencoded);
 
 app.get("/", (req, res) => {
   res.send("Porti Backend Music Store");
 });
+
+// Routes
+app.use("/musica", require("./routes/musica.routes"));
 
 module.exports = app;
--- src/routes/musica.routes.js
+++ src/routes/musica.routes.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("Facturas Routes");
+  res.send("Musica Routes");
 });
 
 module.exports = router;
